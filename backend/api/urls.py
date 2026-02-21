2f20e746e735f4d0a9bb5b6c6b724e5924f99fb4
nclvrl <nicolecdverial@gmail.com>
2026-02-21 21:27:31 +0800

    Added permission checks to ReviewViewSet and added it to router

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from professors.views import ProfessorViewSet
+from professors.views import ProfessorViewSet, ReviewViewSet
 
 router = DefaultRouter()
 router.register(r"professors",ProfessorViewSet)
+router.register(r"reviews",ReviewViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- backend/professors/views.py
+++ backend/professors/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from .serializers import ProfessorSerializer, ReviewSerializer
 from .models import Professor, Review
 # Create your views here.
@@ -11,3 +12,7 @@ class ProfessorViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(student=self.request.user)
