f471194e6b52c7c4501afa9bef656b5cc10818e4
nclvrl <nicolecdverial@gmail.com>
2026-02-21 20:35:19 +0800

    Added Review model, serializer, view, admin, and migration

--- backend/professors/serializers.py
+++ backend/professors/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from .models import Professor
+from .models import Professor, Review
 
 class ProfessorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Professor
         fields = ["professor_id","f_name","l_name","m_name","email"]
+
+class ReviewSerializer(serializers.ModelSerializer):
+    student_name = serializers.StringRelatedField(source="student", read_only=True)
+    professor_name = serializers.StringRelatedField(source="professor", read_only=True)
+
+    class Meta:
+        model = Review
+        fields = ["review_id", "student", "student_name", "professor", "professor_name",
+            "review_rating", "comment_text", "review_date", "received_grade"]
--- backend/professors/views.py
+++ backend/professors/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import ProfessorSerializer
-from .models import Professor
+from .serializers import ProfessorSerializer, ReviewSerializer
+from .models import Professor, Review
 # Create your views here.
 
 class ProfessorViewSet(viewsets.ModelViewSet):
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
 
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
