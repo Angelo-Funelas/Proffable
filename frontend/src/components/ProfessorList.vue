4ef66da84744e620146c5ecdaa2fa2842bec492d
eevicteen <brycerivero@gmail.com>
2026-02-24 09:18:58 +0800

    Created temporary styling for professor list. To do:
    a.) Separate search bar, left div and filters div into separate components
    b.) Implement dropdown menu for university

--- frontend/src/components/ProfessorList.vue
+++ frontend/src/components/ProfessorList.vue
@@ -28,19 +28,35 @@ import ProfCard from './ProfCard.vue'
 </script>
 
 <template>
-<h1 class="text-5xl font-bold text-left">Professors</h1>
-<p v-if="isLoading">Loading professors...</p>
+<div class="grid grid-cols-[4fr_11fr] gap-x-[30px] w-screen p-[64px]"> 
+    <!--LEFT DIV-->
+    <div class="flex flex-col gap-2 text-left">
+        <input class="rounded-xl bg-[#FFFFFF] form-text mt-[5px] h-[35px]" placeholder="INSERT SEARCH BAR HERE"/>
+        <!--QUERY/FILTERS DIV-->
+        <div class="bg-[#52848A] rounded-xl p-[18px]  flex flex-col gap-2 text-left">
+            <p>UNIVERSITY</p>
+            <p>DEPARTMENT</p>
+        </div>
+    </div>
 
-<ul class="grid grid-cols-1 gap-1">
-    <li  v-for="prof in professors" :key="prof.professor_id">
-        <ProfCard
-        :lname="prof.l_name"
-        :fname="prof.f_name"
-        :avgScore="3"
-        :numReviews="128"
-        />
-    </li>
-</ul>
+    <!--RIGHT DIV-->
+    <div>
+        <h1 class="text-5xl font-bold text-left">Professors</h1>
+    <p v-if="isLoading">Loading professors...</p>
+
+    <ul class="grid grid-cols-1 gap-y-[10px]">
+        <li  v-for="prof in professors" :key="prof.professor_id">
+            <ProfCard
+            :lname="prof.l_name"
+            :fname="prof.f_name"
+            :avgScore="3"
+            :numReviews="128"
+            />
+        </li>
+    </ul>
+    </div>
+</div>
+
 </template>
 
 const props = defineProps({
