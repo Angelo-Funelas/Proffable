bbb4a8a96a0aa5f0833d79c9e1215979a7f094a7
nclvrl <nicolecdverial@gmail.com>
2026-02-27 00:35:03 +0800

    Minor fix on placeholder color

--- frontend/src/components/LoginView.vue
+++ frontend/src/components/LoginView.vue
@@ -16,7 +16,7 @@
             <svg class="h-5 w-5 mr-3 text-[#719294] shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
             </svg>
-            <input type="email" placeholder="Email" required class="flex-grow bg-transparent outline-none text-[#719294] text-sm border-none" />
+            <input type="email" placeholder="Email" required class="flex-grow bg-transparent outline-none text-[#0B0D09] placeholder:text-[#719294] text-sm border-none" />
           </div>
 
           <div class="flex items-center border-b border-[#719294] py-2 focus-within:border-[#5c898d] transition-colors">
@@ -27,14 +27,14 @@
               :type="isPasswordVisible ? 'text' : 'password'" 
               placeholder="Password" 
               required
-              class="flex-grow bg-transparent outline-none text-[#719294] text-sm border-none" 
+              class="flex-grow bg-transparent outline-none text-[#0B0D09] placeholder:text-[#719294] text-sm border-none" 
             />
             <button @click="togglePassword" type="button" class="shrink-0 p-1 flex items-center cursor-pointer border-none !bg-white rounded-md shadow-sm outline-none hover:!bg-[#719294] transition-colors">
-              <svg v-if="!isPasswordVisible" class="h-5 w-5 text-[#719294]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <svg v-if="!isPasswordVisible" class="h-5 w-5 text-[#0B0D09]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
               </svg>
-              <svg v-else class="h-5 w-5 text-[#719294]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <svg v-else class="h-5 w-5 text-[#0B0D09]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.542-7a9.97 9.97 0 011.563-3.076m1.903-1.903A9.952 9.952 0 0112 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3l18 18" />
               </svg>
