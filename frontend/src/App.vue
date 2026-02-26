cd4b5b3db687759f7d406f3de3e4704530ec8dd6
rblugrwblc <robiniquecacho04@gmail.com>
2026-02-26 15:32:43 +0800

    fix: endpoint routing for prof-details found at `/professor/index` and prof-list found at `/professors`

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,4 +6,5 @@
 import ProfessorList from './components/ProfessorList.vue';
 import ReviewForm from './components/ReviewForm.vue';
 import LoginView from './components/LoginView.vue'; 
+import ProfessorDetail from './components/ProfessorDetail.vue'; 
 </script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ProfessorList from '../components/ProfessorList.vue'
 import ReviewForm from '../components/ReviewForm.vue'
 import LoginView from '../components/LoginView.vue'
+import ProfessorDetail from '../components/ProfessorDetail.vue'
 
 const routes = [
   { path: '/professors', component: ProfessorList },
+  { path: '/professor/1', component: ProfessorDetail },
   { path: '/reviews/:professorId', component: ReviewForm },
   { path: '/login', component: LoginView }
 ]
