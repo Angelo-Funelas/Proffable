7e53461e36018e24775cac3d1acd00da01848df2
rblugrwblc <robiniquecacho04@gmail.com>
2026-02-27 00:51:24 +0800

    add: logic and endpoints for GET and POST requests for user information

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,6 +7,16 @@ router = DefaultRouter()
 router.register(r"professors",ProfessorViewSet)
 router.register(r"reviews",ReviewViewSet)
 
+#include logic for jwt 
+from .serializers import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from .views import me
+from .views import register_user
+
 urlpatterns = [
+    path("token/", CustomTokenObtainPairView.as_view()),
+    path("token/refresh/", TokenRefreshView.as_view()),
+    path("register/", register_user),
+    path("me/", me),
     path('', include(router.urls)),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Import the things to function 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+# Entity User
+from api.models import User
+
+# Conver to JSON
+from .serializers import RegisterSerializer
+
+# GET request for user information
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def me(request):
+    user = request.user
+    return Response({
+        "email": user.email,
+        "f_name": user.f_name,
+        "l_name": user.l_name,
+        "is_moderator": user.is_moderator,
+    })
+
+# POST request for making a new user 
+@api_view(["POST"])
+def register_user(request):
+    data = request.data
+    user = User.objects.create_user(
+        username=data["username"],
+        email=data["email"],
+        password=data["password"],
+        f_name=data.get("f_name", ""),
+        l_name=data.get("l_name", "")
+    )
+
+    
+    return Response({"message": "User created"}, status=status.HTTP_201_CREATED)
