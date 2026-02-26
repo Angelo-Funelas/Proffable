c3f68062ae93d04b380f0a257628afbb91bb4a49
Angelo-Funelas <angelomartinfunelas@gmail.com>
2026-02-27 02:02:36 +0800

    Setup polling and HMR for development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-    tailwindcss(),],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
+  server: {
+    host: true,         // Allows access from outside the container
+    port: 5173,
+    watch: {
+      usePolling: true, // Required for Docker on Windows/WSL to see file changes
+    },
+    hmr: {
+      clientPort: 5173, // Ensures the browser connects to the right HMR port
+    },
+  },
 })
