00d28bc5d2ee7b6d7581359151d2a613d4b65c92
nclvrl <nicolecdverial@gmail.com>
2026-02-26 23:49:58 +0800

    Fixed professor list accdg to figma

--- frontend/src/components/SearchFilters.vue
+++ frontend/src/components/SearchFilters.vue
@@ -1,7 +1,7 @@
 <template> 
 
 <div class="flex flex-col gap-2 text-left">
-        <input class="rounded-2xl bg-[#FFFFFF] form_text mt-[5px] h-[35px] text-center " placeholder="Search for a professor or course"/>
+        <input class="rounded-2xl bg-[#FFFFFF] form_text mt-[5px] h-[35px] px-3 text-[#719294] " placeholder="Search for a professor or course"/>
         <!--QUERY/FILTERS DIV-->
 
         <!--NOTE: THE FUNCTIONS BELOW ARE OUT OF THE SCOPE IN CURRENT ITERATION AND ARE ONLY PLACEHOLDERS-->
@@ -27,7 +27,7 @@
         </div>
         </div>
 
-        <button class="bg-[#52848A] rounded-xl px-[18px] w-max justify-center mx-auto">
+        <button class="bg-[#52848A] rounded-full px-[18px] py-1 w-max justify-center mx-auto">
             Add a Professor
         </button>
         
