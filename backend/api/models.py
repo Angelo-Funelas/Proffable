f11ecdba88302991a0ad47bddec43afb54bfe6dd
rblugrwblc <robiniquecacho04@gmail.com>
2026-02-27 01:14:46 +0800

    add: ignore `.env` files

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,11 +6,10 @@ class CustomUserManager(UserManager):
         return self.get(username__iexact=username)
 
 class User(AbstractUser):
-    # institution = models.ForeignKey(Institution, on_delete=models.SET_NULL, related_name="students") uncomment once Institution model is created
-    f_name = models.CharField(blank="False", max_length=32)
-    l_name = models.CharField(blank="False", max_length=32)
-    m_name = models.CharField(blank="True", max_length=32)
-    profile_picture_url = models.CharField(blank="True", max_length=128)
+    f_name = models.CharField(blank=False, max_length=32)
+    l_name = models.CharField(blank=False, max_length=32)
+    m_name = models.CharField(blank=True, max_length=32)
+    profile_picture_url = models.CharField(blank=True, max_length=128)
     is_moderator = models.BooleanField(default=False)
     email = models.EmailField(unique=True, max_length=254)
     objects = CustomUserManager()
