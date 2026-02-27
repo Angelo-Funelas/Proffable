ad643788da68826962f1f61821f38d2f4bf56b0f
rblugrwblc <robiniquecacho04@gmail.com>
2026-02-27 10:46:26 +0800

    fix: AllowAll for READ requests in ReviewViewSet

--- backend/professors/views.py
+++ backend/professors/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import ProfessorSerializer, ReviewSerializer
 from .models import Professor, Review
 # Create your views here.
@@ -12,7 +12,7 @@ class ProfessorViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(student=self.request.user)
