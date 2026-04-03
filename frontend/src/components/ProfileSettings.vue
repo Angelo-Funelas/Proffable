dcb0acc41b2d4142df2c6c1a209f969b52cbb352
nclvrl <nicolecdverial@gmail.com>
2026-04-03 14:13:12 +0800

    Integrated profile fetch api

--- frontend/src/components/ProfileSettings.vue
+++ frontend/src/components/ProfileSettings.vue
@@ -1,3 +1,50 @@
+<script setup>
+    import { ref, onMounted } from "vue"
+    import { useRouter } from "vue-router"
+    import api from "@/api/axios"
+
+    const router = useRouter()
+
+    const form = ref({
+        username: "",
+        email: "",
+        f_name: "",
+        m_name: "",
+        l_name: "",
+        profile_picture_url: "",
+        password: "",
+    })
+
+    const message = ref("")
+
+    const fetchProfile = async () => {
+    try {
+        const res = await api.get("me/")
+        form.value = { ...form.value, ...res.data }
+    } catch (err) {
+        console.error(err)
+    }
+    }
+
+    const updateProfile = async () => {
+        try {
+            await api.patch("me/update/", form.value)
+            message.value = "Profile updated successfully!"
+            if (form.value.password) {
+                localStorage.removeItem("access")
+                router.push("/login")
+            }
+        } catch (err) {
+            console.error(err)
+            message.value = "Error updating profile"
+        }
+    }
+
+    onMounted(() => {
+        fetchProfile()
+    })
+</script>
+
 <template>
     <h1>Hello, world!</h1>
 </template>
