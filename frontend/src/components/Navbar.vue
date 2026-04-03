c8f78f2828468eea084db8be1ea4b30ac4e73232
nclvrl <nicolecdverial@gmail.com>
2026-04-03 16:33:22 +0800

    Added user dropdown to the navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
+import api from "@/api/axios"
+
 
 const router = useRouter()
 
@@ -66,13 +68,51 @@ const logout = () => {
       <img src="../assets/ProffableLogo.png" alt="Logo" class="h-7 w-7 object-contain pointer-events-none" />
     </div>
 
-    <button
-      v-if="isAuthenticated"
-      @click="logout"
-      class="text-white font-semibold hover:opacity-80 transition"
-    >
-      Logout
-    </button>
+
+    <div v-if="isAuthenticated" class="relative">
+      <div
+        @click="toggleDropdown"
+        class="flex items-center gap-2 cursor-pointer text-white font-semibold"
+      >
+        <img
+          v-if="user?.profile_picture_url"
+          :src="user.profile_picture_url"
+          class="h-8 w-8 rounded-full object-cover"
+        />
+
+        <div
+          v-else
+          class="h-8 w-8 rounded-full bg-[#d9d9d9] flex items-center justify-center text-sm text-black"
+        >
+          {{ user?.f_name?.[0] || "U" }}
+        </div>
+
+        <span>
+          Welcome, {{ user?.f_name || "User" }}!
+        </span>
+
+        <span>▼</span>
+      </div>
+
+      <div
+        v-if="dropdownOpen"
+        class="absolute right-0 mt-2 w-40 bg-white rounded-lg shadow-md text-black"
+      >
+        <button
+          @click="goToProfile"
+          class="block w-full text-left px-4 py-2 hover:bg-gray-100"
+        >
+          Profile
+        </button>
+
+        <button
+          @click="logout"
+          class="block w-full text-left px-4 py-2 hover:bg-gray-100"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
 
     <button
       v-else
