dd4f8ac409c4b929acdd2b4bc3465eb9ccc075ee
Angelo-Funelas <angelomartinfunelas@gmail.com>
2026-04-13 00:22:28 +0800

    Removed domain field in institution model

--- backend/professors/models.py
+++ backend/professors/models.py
@@ -8,7 +8,6 @@ from django.conf import settings
 class Institution(models.Model):
     institution_id = models.AutoField(primary_key=True)
     name = models.CharField(blank=False, max_length=255)
-    domain = models.CharField(blank=False, max_length=255)
     def __str__(self):
         return f"{self.name}"
 
