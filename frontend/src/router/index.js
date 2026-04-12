13c6f52699bb3e761bd1b996167cf5886dd05324
Angelo-Funelas <angelomartinfunelas@gmail.com>
2026-04-12 17:34:50 +0800

    Added favicon and titles

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,19 +9,45 @@ import ProfileSettings from '../components/ProfileSettings.vue'
 import ModeratorView from '../components/ModeratorView.vue'
 
 const routes = [
-  { path: '/moderation', component: ModeratorView },
+  { path: '/moderation', component: ModeratorView, meta: {
+    title: "Moderator Dashboard"
+  }},
   { path: '/professors', component: ProfessorList },
   { path: '/professor/:professorId', component: ProfessorDetail },
   { path: '/reviews/:professorId', component: ReviewForm },
-  { path: '/login', component: LoginView },
-  { path: '/sign-up', component: Register},
-  { path: '/profile', component: ProfileSettings },
-  { path: '/', component: HomePage }
+  { path: '/login', component: LoginView, meta: {
+    title: "Login to Proffable"
+  } },
+  { path: '/sign-up', component: Register, meta: {
+    title: "Join Proffable"
+  }},
+  { path: '/profile', component: ProfileSettings, meta: {
+    title: "Proffable - Profile Settings"
+  } },
+  { path: '/', component: HomePage, meta: {
+    title: "Proffable | Professor Ratings & Reviews"
+  } }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const siteTitle = to.meta.title || 'Proffable';
+  document.title = siteTitle;
+
+  const favicon = to.meta.favicon || "/ProffableLogo.png";
+  let link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+  link.href = favicon;
+
+  next();
+});
 
 export default router
